ff57ae785d1b5979461169fd57cd89705a6285f9
Hans Jákup Danielsen <hansjd@hotmail.com>
2021-12-01 18:27:34 +0000

    added day picker

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("select a day!");
 
-Day1 day1 = new Day1();
-day1.Run();
+if(int.TryParse(Console.ReadLine(), out int input))
+{
+    switch (input)
+    {
+        case 1:
+            _ = new Day1();
+            break;
+        default:
+            break;
+    }
+}
