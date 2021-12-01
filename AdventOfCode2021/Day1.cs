c4f8f4c4c82f51b1e850bfc71fe1c114629b1fb6
Hans Jákup Danielsen <hansjd@hotmail.com>
2021-12-01 18:15:55 +0000

    making code leaner

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -1,50 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 internal class Day1
 {
-    private readonly List<int> input;
-    public int NumberOfTimesIncreased { get; set; }
-    public int NumberOfTimesDecreased { get; set; }
-    public int NumberOfTimesEqual { get; set; }
-
     public Day1()
     {
-        input = File.ReadAllLines("Input/01.txt").Select(i => int.Parse(i)).ToList(); ;
-    }
+        List<int> input = File.ReadAllLines("Input/01.txt").Select(i => int.Parse(i)).ToList();
+        int NumberOfTimesIncreased = 0;
 
-    internal void Run()
-    {
-        int? previousDepth = null;
-        for (int i = 1; i < input.Count - 1; i++)
+        for (int i = 2; i < input.Count - 1; i++)
         {
+            int j = i - 1;
+            int previousDepth = input[j - 1] + input[j] + input[j + 1];
             int currentDepth = input[i - 1] + input[i] + input[i + 1];
 
-            if (previousDepth is null)
-            {
-                Console.WriteLine($"{currentDepth} (N/A - no previous measurement)");
-                previousDepth = currentDepth;
-                continue;
-            }
-
-
             if (currentDepth > previousDepth)
-            {
-                Console.WriteLine($"{currentDepth} (increased)");
                 NumberOfTimesIncreased++;
-            }
-            else if (currentDepth < previousDepth)
-            {
-                Console.WriteLine($"{currentDepth} (decreased)");
-                NumberOfTimesDecreased++;
-            }
-            else
-            {
-                Console.WriteLine($"{currentDepth} (no change)");
-                NumberOfTimesEqual++;
-            }
             previousDepth = currentDepth;
         }
 
         Console.WriteLine($"Number of time greater than previous depth: {NumberOfTimesIncreased}");
-
     }
 }
