4b3a1668ad70ddde6b8f05b066d31a81a82f4c36
Hans Jákup Danielsen <hjd@formula.fo>
2021-12-01 16:01:47 +0000

    Add project files.

---
+++ AdventOfCode2021/Day1.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Day1
+{
+    private readonly List<int> input;
+    public int NumberOfTimesIncreased { get; set; }
+    public int NumberOfTimesDecreased { get; set; }
+    public int NumberOfTimesEqual { get; set; }
+
+    public Day1()
+    {
+        input = File.ReadAllLines("Input/01.txt").Select(i => int.Parse(i)).ToList(); ;
+    }
+
+    internal void Run()
+    {
+        DoPart1();
+        //DoPart2();
+
+    }
+
+    private void DoPart2()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DoPart1()
+    {
+        int? previousDepth = null;
+        for (int i = 1; i < input.Count-1; i++)
+        {
+            int currentDepth = input[i-1]+input[i]+input[i+1];
+
+            if (previousDepth is null)
+            {
+                Console.WriteLine($"{currentDepth} (N/A - no previous measurement)");
+                previousDepth = currentDepth;
+                continue;
+            }
+
+
+            if (currentDepth > previousDepth)
+            {
+                Console.WriteLine($"{currentDepth} (increased)");
+                NumberOfTimesIncreased++;
+            }
+            else if (currentDepth < previousDepth)
+            {
+                Console.WriteLine($"{currentDepth} (decreased)");
+                NumberOfTimesDecreased++;
+            }
+            else
+            {
+                Console.WriteLine($"{currentDepth} (no change)");
+                NumberOfTimesEqual++;
+            }
+            previousDepth = currentDepth;
+        }
+
+        Console.WriteLine($"Number of time greater than previous depth: {NumberOfTimesIncreased}");
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Day1 day1 = new Day1();
+day1.Run();
